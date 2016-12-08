166094aab0938aa1726e76f327c0ac17a8b39df7
Kevin Evans <kevev243@student.liu.se>
2016-12-08 13:56:19 +0100

    added more css for startpage

--- startsida.php
+++ startsida.php
@@ -9,9 +9,20 @@
 		<link href="utseende.css" rel="stylesheet" type="text/css"/>
 	</head>
 	<body>
-		<div id="startContainer">
-			<h1 id="startHeader">welcome to <span>Lego Database Finder<span></h1>
+		<div class="colContainer">
+			<div id="startContainer">
+				<h1 id="startHeader">welcome to <span>Lego Database Finder<span></h1>
+			</div>
+			<div id="searchTabContainer">
+				<div>
+					<h3>Bit</h3>
+				</div>
+				<div>
+					<h3>Sats</h3>
+				</div>
+			</div>
 		</div>
-		
+
+
 
 <!--- NOT END OF FILE. NEED TO INCLUDE --->
