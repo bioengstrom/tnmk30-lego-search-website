878824034969b57a718f29d9ef2fa9fb3608fc7a
Vera Fristedt Andersson <verafristedt@c-124-164.eduroam.liu.se>
2016-12-08 13:42:40 +0100

    de två olika sidorna

---
+++ search_bit.php
@@ -0,0 +1,19 @@
+<?php include("startsida.php"); ?>
+	
+	<div>
+		<!-- Här ska bitens bild in, bredvid ska namnet på biten finnas, id-nummer 
+		och en drop-down meny med färgalternativ som ska ändra färg på bilden vid 
+		tryckning.-->
+	</div>
+	<div>
+		<p>This item is part of ... sets:</p>
+		<table>
+			<tr>
+				<td>(bild)</td>
+				<td><p>Setname</p><p>Id-number</p></td>
+			</tr>
+		</table>
+	</div>
+	
+	</body>
+</html>
