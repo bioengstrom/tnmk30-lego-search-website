1a614a7aa49b60ba1b9188d9f1da5ad7090f7278
Kevin Evans <kevev243@student.liu.se>
2016-12-08 11:44:23 +0100

    changed start to php

---
+++ startsida.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<!doctype html>
+<html>
+	<head>
+		<title>Lego DB searcher 2000</title>
+		<link href="utseende.css" rel="stylesheet" type="text/css"/>
+	</head>
+	<body>
+		<h1 id="startHeader">Welcome to Lego Database finder</h1>
+	</body>
+</html>
