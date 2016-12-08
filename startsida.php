ff380a378eadb55deb1a7f2fcba4512b10da84ef
Kevin Evans <kevev243@student.liu.se>
2016-12-08 12:23:21 +0100

    started design of header

--- startsida.php
+++ startsida.php
@@ -5,9 +5,13 @@
 <html>
 	<head>
 		<title>Lego DB searcher 2000</title>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700" rel="stylesheet">
 		<link href="utseende.css" rel="stylesheet" type="text/css"/>
 	</head>
 	<body>
-		<h1 id="startHeader">Welcome to Lego Database finder</h1>
-	</body>
-</html>
+		<div id="startContainer">
+			<h1 id="startHeader">welcome to <span>Lego Database Finder<span></h1>
+		</div>
+		
+
+<!--- NOT END OF FILE. NEED TO INCLUDE --->
