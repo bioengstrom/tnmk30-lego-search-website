3e7107d611bb323c8134e82651b1658aa616fc01
unknown <kevev243@ITN-TP4004-01.ad.liu.se>
2016-12-12 15:30:06 +0100

    more desiiiiign

--- validator.js
+++ validator.js
@@ -1,32 +1,36 @@
 function runFunction() {
-  setTimeout(setInterval(checkLength, 1000), 5000);
+  setTimeout(startInterval, 2000);
 }
-
+function startInterval() {
+  setInterval(checkLength, 10);
+}
 function checkLength() {
     var keyword = document.getElementById("keyword");
     if(keyword.value.length >= 3 || keyword.value.length == 0){
-        var interval = setInterval(display("block"), 500);
+        var interval = setInterval(display("block"), 1);
     }
     else if (keyword.value.length != 0){
-        var interval = setInterval(display("none"), 500);
+        var interval = setInterval(display("none"), 1);
     }
 }
-
+var opacity = 0;
 function display(visual) {
   var alert = document.getElementsByClassName("alertWarning")[0];
   if (visual == "block") {
     console.log("waddup");
-    var opacity = 0;
-    opacity += 0.10;
-    alert.style.display = "none";
-    alert.style.opacity = opacity;
+    if (opacity >= 0) {
+        opacity -= 0.05;
+    }
+    alert.style.opacity = opacity + "";
   }
   else if (visual == "none") {
     console.log("boi");
-    var opacity = 1;
-    opacity -= 0.10;
+    if (opacity <= 1) {
+        opacity += 0.05;
+    }
+
     alert.style.display = "block";
-    alert.style.opacity = "1";
+    alert.style.opacity = opacity +"";
   }
 }
  /*FIX THIS BULLSHIT ASS CODE*/
