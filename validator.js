24d5ec8a499fc9235f971eb5c4f4c7b703e4f70d
Isak Engström <Isak Engstrm>
2016-12-13 12:13:51 +0100

    fixed new animation for character-length error message.

--- validator.js
+++ validator.js
@@ -1,3 +1,56 @@
+//runs the script so that it updates without requiring reload, (aka AJAX?)
+
+function runFunction() {
+  setTimeout(startInterval, 3000);
+}
+function startInterval() {
+  setInterval(checkLength, 1000);
+}
+
+//checks length of string entered in searchbar.
+function checkLength() {
+  var keyword = document.getElementById("keyword");
+  if(keyword.value.length >= 3 || keyword.value.length == 0){
+    display("block");
+  }
+  else { //if (keyword.value.length != 0)
+    display("none");
+  }
+}
+//reveals error message if entered string is too short.
+function display(visual) {
+  var alert = document.getElementsByClassName("alertWarning")[0];
+  if (visual == "block") {
+    console.log("length >= 3 || length == 0");
+    if(alert.style.opacity == "1") { //Error message showing? if no, don't fadeOut.
+      var fadeOutTrig = "fadeOut 0.5s";
+      //takes care of animation for different browser variations
+      alert.style.webkitAnimation = fadeOutTrig;
+      alert.style.MozAnimation = fadeOutTrig;
+      alert.style.msAnimation = fadeOutTrig;
+      alert.style.OAnimation = fadeOutTrig;
+      alert.style.animation = fadeOutTrig;
+
+      alert.style.opacity = "0"; //keeps error message OFF
+    }
+  }
+  else if (visual == "none") {
+    console.log("0 < length < 3");
+    var fadeInTrig = "fadeIn 0.5s";
+    //takes care of animation for different browser variations
+    alert.style.webkitAnimation = fadeInTrig;
+    alert.style.MozAnimation = fadeInTrig;
+    alert.style.msAnimation = fadeInTrig;
+    alert.style.OAnimation = fadeInTrig;
+    alert.style.animation = fadeInTrig;
+
+    alert.style.opacity = "1"; //keeps error message ON
+  }
+}
+
+/*-------------------------OLD VERSION-------------------------*/
+
+/*
 function runFunction() {
   setTimeout(startInterval, 2000);
 }
@@ -39,4 +92,4 @@ function display(visual) {
     alert.style.opacity = opacity +"";
   }
 }
- /*FIX THIS BULLSHIT ASS CODE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+*/
