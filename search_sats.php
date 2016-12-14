01bbf7feaca556463f84efc10f35b32ae17c2887
Isak Engström <isaen688@student.liu.se>
2016-12-14 11:40:18 +0100

    ny choose_set sida

--- search_sats.php
+++ search_sats.php
@@ -17,20 +17,39 @@
 									AND sets.SetID=inventory.SetID AND inventory.ItemID=parts.PartID AND
 									inventory.ColorID=colors.ColorID ORDER BY Partname");
 
+			$link = "http://weber.itn.liu.se/~stegu76/img.bricklink.com";
+
+			$setsearch = mysqli_query($connection, "SELECT SetID, Setname FROM sets WHERE (SetID LIKE '%$keyword%' OR Setname LIKE '%$keyword%')");
+
+			while($setinfo = mysqli_fetch_array($setsearch) AND $keyword != NULL) {
+
+				$SetID = $setinfo['SetID'];
+
+				print("<div id='legoItem'>");
+				$imagesearch = mysqli_query($connection, "SELECT * FROM images WHERE ItemID='$SetID' AND ItemTypeID = 'S'");
+			// By design, the query above should return exactly one row.
+				$imageinfo = mysqli_fetch_array($imagesearch);
+					if($imageinfo['has_largejpg']) { // Use JPG if it exists
+							$filename = "$link/SL/$SetID.jpg";
+					} else if($imageinfo['has_largegif']) { // Use GIF if JPG is unavailable
+							$filename = "$link/SL/$SetID.gif";
+					} else { // If neither format is available, insert a placeholder image
+							$filename = "error.png";
+					}
+					echo "<img class='setImg' src='".$filename."'></img>";
+
+					echo "<div class='infoText'><p class='legoName'>".$setinfo["Setname"]."</p>
+					 			<p class='legoID'>ID-number: ".$setinfo["SetID"]."</p></div>";
+				 		print ("</div>");
+					}
+
 			print("<p id ='amountParts'>This item consists of:</p>
 			<div id='allParts'>");
-
-			// print("<div id='legoItem'>");
-				// echo "<div class='infoText'><p class='legoName'>".$row["Setname"]."</p>
-					// <p class='legoID'>".$row["SetID"]."</p></div>";
-				// print ("</div>");
-
 
 			while($row = mysqli_fetch_array($result) AND $keyword != NULL)
 			{
 					print ("<div class='legoPart'>");
 
-					$link = "http://weber.itn.liu.se/~stegu76/img.bricklink.com";
 					$ItemID = $row['ItemID'];
 					$ColorID = $row['ColorID'];
 					$imagesearch = mysqli_query($connection, "SELECT * FROM images WHERE ItemTypeID='P' AND ItemID='$ItemID' AND ColorID=$ColorID");
@@ -44,44 +63,16 @@
 				 	 			$filename = "error.png";
 				    }
 
-
 				  echo "<img class='partImg' src='".$filename."'></img>";
-					echo "<div class='infoText'><p>", $row["Partname"], "</p><p>", $row["PartID"], "</p><p>",
+					echo "<div class='infoText'><p>", $row["Partname"], "</p><p>ID-number: ", $row["PartID"], "</p><p>Color: ",
 					$row["Colorname"], "</p><p>Quantity: ", $row["Quantity"], "</p></div>";
 					print ("</div>");
-
 			}
 
 			print ("</div>");
 			mysqli_close($connection);
-
 		?>
 
-			<div id="legoItem">
-				<!-- Här ska bitens bild in, bredvid ska namnet på biten finnas, id-nummer
-				och en drop-down meny med färgalternativ som ska ändra färg på bilden vid
-				tryckning.-->
-				<img src="http://1.bp.blogspot.com/-23j6MHmmuto/T3qT9y4oItI/AAAAAAAAALc/-UYN6YSdZLM/s1600/Lego-Brick-4x2.jpg"></img>
-				<div class="infoText">
-					<p class="legoName">Namn</p>
-					<p class="legoID">ID</p>
-				</div>
-			</div>
-			<p id ="amountParts">This item consists of:</p>
-			<div id="allParts">
-				<div class="legoPart">
-					<img src="http://1.bp.blogspot.com/-23j6MHmmuto/T3qT9y4oItI/AAAAAAAAALc/-UYN6YSdZLM/s1600/Lego-Brick-4x2.jpg"></img>
-					<div class="infoText">
-						<p>Partname</p>
-						<p>Id-number</p>
-						<p>Antal bitar</p>
-					</div>
-				</div>
-
-			</div>
-
 	</div>
-
-
 	</body>
 </html>
