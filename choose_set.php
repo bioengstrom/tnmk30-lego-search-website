f3dff04020126134d2f15a8fb7102db32d855d59
Vera Andersson <veran877@student.liu.se>
2016-12-14 13:10:20 +0100

    länken från choose_php till search_php funkar

--- choose_set.php
+++ choose_set.php
@@ -3,46 +3,50 @@
   <div id="itemContainer">
 
     <?php
-      $connection = mysqli_connect("mysql.itn.liu.se", "lego", "", "lego");
+		$connection = mysqli_connect("mysql.itn.liu.se", "lego", "", "lego");
 
-      if (!$connection) {
-        die('MySQL connection error');
-      }
+		if (!$connection) {
+			die('MySQL connection error');
+		}
 
-      $keyword = mysqli_real_escape_string($connection, $_POST["keyword"]);
+		$keyword = mysqli_real_escape_string($connection, $_POST["keyword"]);
 
-      $result = mysqli_query($connection, "SELECT Setname, SetID FROM sets WHERE (sets.SetID LIKE '%$keyword%' OR sets.Setname LIKE '%$keyword%')
-                              ORDER BY Setname");
+		$result = mysqli_query($connection, "SELECT Setname, SetID FROM sets WHERE (sets.SetID LIKE '%$keyword%' OR sets.Setname LIKE '%$keyword%')
+								ORDER BY Setname");
+	
+		$link = "http://weber.itn.liu.se/~stegu76/img.bricklink.com";
 
-      $link = "http://weber.itn.liu.se/~stegu76/img.bricklink.com";
+		print("<p id ='amountParts'>These sets contain the keyword: ".$keyword."</p>
+				<div id='allParts'>");
 
-      print("<p id ='amountParts'>These sets contain the keyword: ".$keyword."</p>
-            <div id='allParts'>");
+		while($row = mysqli_fetch_array($result) AND $keyword != NULL){
 
-      while($row = mysqli_fetch_array($result) AND $keyword != NULL)
-			{
-					print ("<div class='legoPart'>");
+			print ("<div class='legoPart'>");
 
-					$ItemID = $row['SetID'];
+			$SetID = $row['SetID'];
+			$Setname = $row['Setname'];
 
-					$imagesearch = mysqli_query($connection, "SELECT * FROM images WHERE ItemTypeID='S' AND ItemID='$ItemID'");
-			    // By design, the query above should return exactly one row.
-			    $imageinfo = mysqli_fetch_array($imagesearch);
-				    if($imageinfo['has_jpg']) { // Use JPG if it exists
-				 	 			$filename = "$link/S/$ItemID.jpg";
-				    } else if($imageinfo['has_gif']) { // Use GIF if JPG is unavailable
-				 	 			$filename = "$link/S/$ItemID.gif";
-				    } else { // If neither format is available, insert a placeholder image
-				 	 			$filename = "error.png";
-				    }
-
-				  echo "<div class='imgContainer'><img src='".$filename."'></div>";
-
-					echo "<div class='infoText'><p>", $row["Setname"], "</p><p>ID-number: ", $row["SetID"], "</p></div>";
-          print ("</div>");
+			$imagesearch = mysqli_query($connection, "SELECT * FROM images WHERE ItemTypeID='S' AND ItemID='$SetID'");
+			    
+			$imageinfo = mysqli_fetch_array($imagesearch);
+			
+			if($imageinfo['has_jpg']) { // Use JPG if it exists
+				$filename = "$link/S/$SetID.jpg";
+			} else if($imageinfo['has_gif']) { // Use GIF if JPG is unavailable
+				$filename = "$link/S/$SetID.gif";
+			} else { // If neither format is available, insert a placeholder image
+				 $filename = "error.png";
 			}
-      print ("</div>");
-      mysqli_close($connection);
+
+			echo "<div class='imgContainer'><img src='".$filename."'></div>";
+
+			echo "<div class='infoText'><p><a href='search_sats.php?SetID=".$SetID."'>
+			".$Setname."</a></p><p>ID-number: ".$SetID."</p></div>";
+			print ("</div>");
+		}
+		
+		print ("</div>");
+		mysqli_close($connection);
     ?>
 
   </div>
