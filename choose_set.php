f28ccd5e25c1e6f492e18866b5e1932c4d044831
Kevin Evans <kevev243@student.liu.se>
2016-12-14 14:54:13 +0100

    improved css for choose_set.php

--- choose_set.php
+++ choose_set.php
@@ -13,23 +13,20 @@
 
 		$result = mysqli_query($connection, "SELECT Setname, SetID FROM sets WHERE (sets.SetID LIKE '%$keyword%' OR sets.Setname LIKE '%$keyword%')
 								ORDER BY Setname");
-	
-		$link = "http://weber.itn.liu.se/~stegu76/img.bricklink.com";
 
-		print("<p id ='amountParts'>These sets contain the keyword: ".$keyword."</p>
-				<div id='allParts'>");
+		$link = "http://weber.itn.liu.se/~stegu76/img.bricklink.com";
+    echo "<p id ='amountParts'><span>These sets contain the keyword: </span>".$keyword."</p>";
+    echo "<div id='allParts'>";
 
 		while($row = mysqli_fetch_array($result) AND $keyword != NULL){
-
-			print ("<div class='legoPart'>");
 
 			$SetID = $row['SetID'];
 			$Setname = $row['Setname'];
 
 			$imagesearch = mysqli_query($connection, "SELECT * FROM images WHERE ItemTypeID='S' AND ItemID='$SetID'");
-			    
+
 			$imageinfo = mysqli_fetch_array($imagesearch);
-			
+
 			if($imageinfo['has_jpg']) { // Use JPG if it exists
 				$filename = "$link/S/$SetID.jpg";
 			} else if($imageinfo['has_gif']) { // Use GIF if JPG is unavailable
@@ -38,17 +35,43 @@
 				 $filename = "error.png";
 			}
 
-			echo "<div class='imgContainer'><img src='".$filename."'></div>";
+      /*BEGIN PRINTING OUTPUT*/
+      /* output for one one set*/
+      echo "<a class='legoSet' href='search_sats.php?SetID=".$SetID."'>";
+      echo "<div>";
+			echo "<img src='".$filename."'>";
+			echo "<span>
+              <p class='legoSetTitle'>".$Setname."</p>
+              <p class='legoSetId'>id: ".$SetID."</p>
+            </span>";
+      echo "</div>";
 
-			echo "<div class='infoText'><p><a href='search_sats.php?SetID=".$SetID."'>
-			".$Setname."</a></p><p>ID-number: ".$SetID."</p></div>";
-			print ("</div>");
+			//echo "</div>"; //close allParts div
 		}
-		
-		print ("</div>");
+
+		print ("</div>"); // close itemContainer div
 		mysqli_close($connection);
     ?>
 
-  </div>
+    <!---<div id="itemContainer">
+      <a href="#" class='legoSet'>
+        <div>
+          <img src="http://placehold.it/100x150">
+          <span>
+            <p class="legoSetTitle">Aqua thingy much wow</p>
+            <p class="legoSetId"><span>id: </span>134134</p>
+          </span>
+
+        </div>
+      </a>
+      <a href="#" class='legoSet'>
+        <div>
+          <img src="http://placehold.it/100x150">
+          <p class="legoSetTitle">Aqua thingy much wow</p>
+          <p class="legoSetId"><span>id: </span>134134</p>
+        </div>
+      </a>
+    </div>
+  </div> -->
   </body>
 </html>
